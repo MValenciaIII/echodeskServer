0fadd69583d010dfa6c0cf9c4ab798dc6bb57a88
Charlllieya Smith <charllieya@gmail.com>
2021-04-15 09:22:39 -0500

    cleaning up server side code. Added middleware(multer).

--- app/dao/fileDao.js
+++ app/dao/fileDao.js
@@ -31,144 +31,63 @@ class FileDao {
       res.json(rows[0]);
     });
   }
-  // create(req, res) {
-  //   let fields = Object.keys(req.body);
-  //   // fields[ fields.indexOf('condition')] = 'condition'; //if i were using my cars database
-  //   let values = Object.values(req.body);
-  //   //Required Min Data
-  //   if (
-  //     !req.body.file_name ||
-  //     !req.body.ticket_id 
-  //   ) {
-  //     res.json({
-  //       error: true,
-  //       message: 'ERROR! There is missing data in this form!',
-  //     });
-  //   }
-  // // var file = req.body.file_name;
-  // //   if(file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif"){
-  // //     file.mv('./app/Images/' + file.file_name, function(err){
-  // //       if(err){
-  // //         res.json({
-  // //           error:true,
-  // //           message: err,
-  // //         });
-  // //       }
-  // //     })
-  // //   }
-  //   let sql = `INSERT INTO file(${fields.join(',')})VALUES(${Array(
-  //     values.length
-  //   )
-  //     .fill('?')
-  //     .join(',')});`;
-  //   this.pool.query(
-  //     sql,
-  //     values, //req.body.title, req.body.year, req.body.director_id, req.body.genre_id
-  //     (err, rows) => {
-  //       if (err) {
-  //         res.json({
-  //           error: true,
-  //           message: err,
-  //         });
-  //       }
-  //       res.json(rows);
-  //     }
-  //   );
-  // }
-  // create(req, res) {
 
-  //   if (!req.file_name) {
-  //     console.log("No file upload");
-  // } else {
-  //     console.log(req.file.filename)
-  //     var imgsrc = 'http://locahost:3095/api/files' + req.file_name.filename
-  //     var insertData = "INSERT INTO files(file_name)VALUES(?)"
-  //    pool.query(insertData, [imgsrc], (err, result) => {
-  //         if (err) throw err
-  //         console.log("file uploaded")
-  //     })
-  // }
-  // let fields = Object.keys(req.body);
-  // // fields[ fields.indexOf('condition')] = 'condition'; //if i were using my cars database
-  // let values = Object.values(req.body);
-  // if (
-  //   !req.body.file_name
-  // ) {
-  //   res.json({
-  //     error: true,
-  //     message: 'ERROR! There is missing data in this form!',
-  //   });
-  // } else {
-  //     console.log(req.body_file_name)
-  //     // var imgsrc = 'http://localhost:3095/api/filess' + req.body.file_name
-
-  //   var file = req.body.file_name;
-  //   if(file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif"){
-  //     file.mv('\server\Images' + file.file_name, function(err){
-  //       if(err){
-  //         res.json({
-  //           error:true,
-  //           message: err,
-  //         });
-  //       }
-  //     })
-  //   }
-  //     let sql = `INSERT INTO files(${fields.join(',')})VALUES(${Array(
-  //       values.length
-  //     )
-  //       .fill('?')
-  //       .join(',')});`;
-  //     this.pool.query(
-  //       sql, 
-  //       values, //req.body.title, req.body.year, req.body.director_id, req.body.genre_id
-  //       (err, rows) => {
-  //         if (err) {
-  //           res.json({
-  //             error: true,
-  //             message: err,
-  //           });
-  //         }
-  //         res.json(rows);
-  //       }
-  //     );
-  //   }
-// }
+  uploadFiles(req, res){
+    // debugger;
+    if (!req.file) {
+      console.log('No file upload');
+    } else {
+      var imgsrc = 'http://localhost:4000/' + req.file.filename;
+      console.log({imgsrc})
+      console.log(req.body);
+      console.log(req.body.ticket_id);
+      var insertData = `INSERT INTO files SET file_name = ?, ticket_id = ?`;
+      this.pool.query(insertData, [imgsrc, req.body.ticket_id], (err, result) => {
+        if (err) throw err;
+        console.log('file uploaded');
+        res.send({
+          "code": 200,
+          "success": "file uploaded successfully."
+      })
+      });
+    }
+    console.log(req.body)
+  }
 
 
-
-//   updateById(req, res) {
-//     let fields = Object.keys(req.body); // making dynamic. fields is now an array
-//     // fields[fields.indexOf('condition')] = 'condition'; //if i were using my cars database (for condition because condition is a reserve word in sql) : at position in the array, make equal to backtick array
-//     let values = Object.values(req.body); //making dynmical
+  updateById(req, res) {
+    let fields = Object.keys(req.body); // making dynamic. fields is now an array
+    // fields[fields.indexOf('condition')] = 'condition'; //if i were using my cars database (for condition because condition is a reserve word in sql) : at position in the array, make equal to backtick array
+    let values = Object.values(req.body); //making dynmical
   
-//     if (!req.params.id) {
-//         res.json({
-//             error: true,
-//             message: "Missing ID",
-//         });
-//     } else if (fields.length == 0) {
-//         res.json({
-//             error: true,
-//             message: "No fields to update",
-//         });
-//     }
+    if (!req.params.ticket_id) {
+        res.json({
+            error: true,
+            message: "Missing ID",
+        });
+    } else if (fields.length == 0) {
+        res.json({
+            error: true,
+            message: "No fields to update",
+        });
+    }
   
-//     let sql = `UPDATE clients set ${fields.join("=?,")}=? WHERE id =?`; //update the data!
-//     //have to put =? at the end of the join because join only add between things!
-//     console.log(sql);
+    let sql = `UPDATE files set ${fields.join("=?,")}=? WHERE ticket_id =?`; //update the data!
+    //have to put =? at the end of the join because join only add between things!
+    console.log(sql);
   
-//     this.pool.query(sql, [...values, req.params.id], (err, rows) => {
-//         //... means SPREAD. It takes values from of array (in this instance).
-//         //did this method because, cant send id in body of values. if we didnt use params, then id would have to be passed in last. sent id as a "url param" and that seperated id for the body content.
+    this.pool.query(sql, [...values, req.params.ticket_id], (err, rows) => {
+        //... means SPREAD. It takes values from of array (in this instance).
+        //did this method because, cant send id in body of values. if we didnt use params, then id would have to be passed in last. sent id as a "url param" and that seperated id for the body content.
   
-//         if (err) {
-//             res.json({
-//                 error: true,
-//                 message: err,
-//             });
-//         }
-//         res.json(rows);
-//     });
-//   } 
+        if (err) {
+            res.json({
+                error: true,
+                message: err,
+            });
+        }
+        res.json(rows);
+    });
+  } 
 }
 module.exports = FileDao;
--- index.js
+++ index.js
@@ -1,99 +1,31 @@
-const express = require('express')
-const app = express()
-const bodyparser = require('body-parser')
-//const mysql = require('mysql')
-//const multer = require('multer')
-const path = require('path')
+const express = require('express');
+const app = express();
 const cors = require('cors'); //gives us access to cors. Cors handles the http for us.
-const bodyParser = require('body-parser'); // allows us to parse
 const router = require('./app/routes/router');
-//use express static folder
-var publicDir = require('path').join(__dirname,'./public/Images'); 
-app.use(express.static(publicDir)); 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({
-    extended: true
-}))
-
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true,}));
 app.use(cors());
 
-//app.use(function(req,res, next){
- // res.header("Access-Control-Allow-Origin", "*");
- // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- // next();
-//});
-
- //Database connection
-const db = mysql.createConnection({
-    host: "34.226.17.246",
-    user: "root",
-    password: "memadev",
-    database: "echodeskDev"
-})
-db.connect(function (err) {
-    if (err) {
-   return console.error('error: ' + err.message);
-   }
-   console.log('Connected to the MySQL server.');
-})
-// Use of Multer
-var storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-    callBack(null, './public/Images')     // './public/images/' directory name where save the file
-    },
-   filename: (req, file, callBack) => {
-       callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-   }})
-var upload = multer({
-    storage: storage
-});
-//! Routes start
-
-//@type   GET
-//$route  /
-//@desc   route for Home page
-//@access PUBLIC
-router.get("/create", (req, res) => {
-    res.render('./index.html');
-   res.sendFile(__dirname + "/index.html");
-})
-
-//@type   POST
-//$route  /post
-//@desc   route for post data
-//@access PUBLIC
-router.post("/post", upload.single('fileUpload'), (req, res) => {
-    if (!req.file) {
-      console.log("No file upload");
-   } else {
-        console.log(req.file.filename)
-        var imgsrc = 'http://localhost:4000/' + req.file.filename
-        var insertData = "INSERT INTO files(file_name)VALUES(?)"
-        db.query(insertData, [imgsrc], (err, result) => {
-            if (err) throw err
-            console.log("file uploaded")
-        })
-    }
-}); 
-
-const PORT = 4000; //? Any connection to the react port of 3000 for local hos
+const PORT = 4000; // Any connection to the react port of 4000 for local host
 app.listen(PORT, () => {
   console.log(`Server on PORT: ${PORT}`);
 });
 
 //ALL ROUTES
-
 app.get('/', (req, res) => {
-res.json({ 
- 'All tickets': 'https://mema4kids.info/api/tickets', 
- 'Find by priority': 'https://mema4kids.info/api/priority',
- 'Find by status': 'https://mema4kids.info/api/status',
- 'Find by departments': 'https://mema4kids.info/api/departments',
- 'Find by details': 'https://mema4kids.info/api/details',
- 'Find by service': 'https://mema4kids.info/api/service',
- 'Find by location': 'https://mema4kids.info/api/location',
-})
+  res.sendFile(path.join(__dirname + '/404.html'))
+  // res.json({
+  //   // 'All tickets': 'http://localhost:4000/api/tickets',
+  //   // 'Find by priority': 'http://localhost:4000/api/priority',
+  //   // 'Find by status': 'http://localhost:4000/api/status',
+  //   // 'Find by departments': 'http://localhost:4000/api/departments',
+  //   // 'Find by details': 'http://localhost:4000/api/details',
+  //   // 'Find by service': 'http://localhost:4000/api/service',
+  //   // 'Find by location': 'http://localhost:4000/api/location',
+  // });
 });
 
+app.use('/api', router); //this entire system only exist after /api. system becomes pre-fixed.
 
-app.use('/api', router); //this entire system only exist after /api. pre-fixed with
+
